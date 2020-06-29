4e1d654d5ce76ad2c7a834924a0c85955b83b38c
p788562 <myemail>
2020-06-29 01:43:28 -0300

    Endpoint taxaJuros

---
+++ Api Taxa Juros/src/TaxaJuros.API/Controllers/TaxaJurosController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TaxaJuros.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxaJurosController : ControllerBase
+    {
+        private static readonly double TaxaJuros = 0.01;
+
+        private readonly ILogger<TaxaJurosController> _logger;
+
+        public TaxaJurosController(ILogger<TaxaJurosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public double Get()
+        {
+            return TaxaJuros;
+        }
+    }
+}
