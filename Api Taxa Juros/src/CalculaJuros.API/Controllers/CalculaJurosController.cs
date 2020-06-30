92c54eda01d86451de50345ab0448345bb20a193
p788562 <myemail>
2020-06-30 15:39:12 -0300

    Calcula Juros: obtendo dado da API Taxa Juros.

--- Api Taxa Juros/src/CalculaJuros.API/Controllers/CalculaJurosController.cs
+++ Api Taxa Juros/src/CalculaJuros.API/Controllers/CalculaJurosController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -17,7 +19,7 @@ namespace CalculaJuros.API.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get(double valorInicial, Int32 meses)
+        public async Task<IActionResult> Get(double valorInicial, Int32 meses)
         {
             // Verificação dos parâmetros
             List<string> erros = new List<string>();
@@ -27,9 +29,20 @@ namespace CalculaJuros.API.Controllers
                 erros.Add("Preencha o campo meses com algum valor maior que 0");
             if(erros.Count > 0) 
                 return BadRequest(erros);
-            
+
+            // Obtém taxa de juros
+            double taxajuros = 0.0;            
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://taxajuros.azurewebsites.net/taxajuros"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    taxajuros = Convert.ToDouble(apiResponse);
+                }
+            }
+
             // Cálculo do juros
-            double juros = valorInicial*Math.Pow((1 + 0.01), meses);
+            double juros = valorInicial*Math.Pow((1 + taxajuros), meses);
             
             // Formatação da resposta
             double jurosFormatado = Math.Truncate(100 * juros) / 100;
