f3f2b1bb22b0d4b20f1eca880a32f220d2601e06
p788562 <myemail>
2020-07-02 02:49:02 -0300

    Projeto de testes. Testes de unidade.

---
+++ api-calcular-juros/CalculaJuros.API.Tests/CalculaJuros_CalculaShould.cs
@@ -0,0 +1,55 @@
+using System;
+using Xunit;
+using CalculaJuros.API.Interfaces;  
+using CalculaJuros.API.Services;  
+
+namespace CalculaJuros.API.Tests
+{
+    public class CalculaJuros_CalculaShould
+    {
+        private readonly IJurosService _jurosService;
+
+        public CalculaJuros_CalculaShould()
+        {
+            _jurosService = new JurosService();
+        }
+
+        [Fact]
+        public async void ObtemTaxaJuros_Should_ReturnCorrectValue()
+        {
+            // Act
+            var response = await _jurosService.ObtemTaxaJuros();
+        
+            // Assert
+            Assert.Equal(response, 0.01);
+        }
+
+        [Fact]
+        public void CalculaJuros_Should_ReturnCorrectValue()
+        {
+            // Arrange
+            double valorInicial = 100;
+            int meses = 5;
+            double taxaJuros = 0.01;
+
+            // Act
+            var response = _jurosService.CalculaJuros(valorInicial, meses, taxaJuros);
+        
+            // Assert
+            Assert.Equal(response, 105.10100501000001);
+        }
+
+        [Fact]
+        public void FormataJuros_Should_ReturnCorrectValue()
+        {
+            // Arrange
+            double juros = 105.10100501000001;
+        
+            // Act
+            var response = _jurosService.FormataJuros(juros);
+        
+            // Assert
+            Assert.Equal("105,10", response);
+        }
+    }
+}
